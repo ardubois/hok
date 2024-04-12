2ec6c9161f2e4a412dc5d9dfab7eb68c05246d26
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-04-12 14:50:35 -0300

    teste dynamic loading

--- testecuda/teste_module.cu
+++ testecuda/teste_module.cu
@@ -9,10 +9,9 @@ int main()
 
 printf("inicio.\n");
  void * m_handle = dlopen("module.so", RTLD_NOW);
-  if(m_handle== NULL)  
-      { char message[200];
-        strcpy(message,"Error opening dll!! ");
-      }
+  if (!ext_mod) { 
+   fprintf(stderr, "dlopen failure: %s\n", dlerror()); 
+   exit (EXIT_FAILURE); }
 
 
 printf("m handle %p\n",m_handle);
