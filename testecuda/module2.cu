d3dcf04fcd15242531163f3cce951cb99bee9998
ardubois <ardubois@github.com>
2024-04-16 14:17:46 -0300

    teste dynamic loading

---
+++ testecuda/module2.cu
@@ -0,0 +1,39 @@
+#define gpuErrchk(val) \
+    cudaErrorCheck(val, __FILE__, __LINE__, true)
+void cudaErrorCheck(cudaError_t err, char* file, int line, bool abort)
+{
+    if(err != cudaSuccess)
+    {
+        printf("%s %s %d\n", cudaGetErrorString(err), file, line);
+        if(abort) exit(-1);
+    }
+}
+
+typedef float (*pfunc)(float arg);
+
+__device__ float dev_func(float arg) {
+    return arg * arg;
+}
+
+// create device function pointer here
+__device__ pfunc dev_func_ptr = dev_func;
+
+__global__ void ker_func(pfunc fnc) {
+    // call function through device function pointer
+    printf("%f\n", fnc(2));
+}
+
+extern "C" void launch()
+{
+    // create a host function pointer
+    pfunc host_function_ptr;
+    // copy function pointer value from device to host
+    gpuErrchk(cudaMemcpyFromSymbol(&host_function_ptr, dev_func_ptr, sizeof(pfunc)));
+    // pass the copied function pointer in kernel
+    ker_func<<<1,1>>>(host_function_ptr);
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+    return 0;
+}
