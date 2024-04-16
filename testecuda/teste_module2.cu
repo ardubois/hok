b2aa71537b5e5db7d9acab9d28753c85e6f67f66
ardubois <ardubois@github.com>
2024-04-16 14:56:56 -0300

    teste dynamic loading

--- testecuda/teste_module2.cu
+++ testecuda/teste_module2.cu
@@ -38,7 +38,7 @@ host_function_ptr = get_ptr();
 
 printf("host function pointer main %p", host_function_ptr);
 
-(*launch)(host_function_ptr);
+void (*launch)(host_function_ptr);
 launch= (void(*)())dlsym( m_handle, "launch");
 printf("ok.\n");
 if (launch==NULL) {printf("NULL\n");}
