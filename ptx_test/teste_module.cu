4c4280861f24c27830a4e806623ee58e5cd4e9a2
ardubois <ardubois@github.com>
2024-04-17 14:27:44 -0300

    teste dynamic loading

--- ptx_test/teste_module.cu
+++ ptx_test/teste_module.cu
@@ -2,57 +2,59 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <dlfcn.h>
+#include <cuda.h>
+#include <builtin_types.h>
+
+#define checkCudaErrors(err)  __checkCudaErrors (err, __FILE__, __LINE__)
+
+inline void __checkCudaErrors( CUresult err, const char *file, const int line )
+{
+    if( CUDA_SUCCESS != err) {
+        fprintf(stderr,
+                "CUDA Driver API error = %04d from file <%s>, line %i.\n",
+                err, file, line );
+        exit(-1);
+    }
+}  
+
+
 typedef float (*func)(float);
 
 int main()
 {
 
- void * m_handle = dlopen("./module2.so", RTLD_NOW);
-  if (!m_handle) { 
-   fprintf(stderr, "dlopen failure: %s\n", dlerror()); 
-   exit (EXIT_FAILURE); }
-char *errstr;
-errstr = dlerror();
-if (errstr != NULL)
-printf ("A dynamic linking error occurred: (%s)\n", errstr);
+CUdevice   device;
+CUcontext  context;
+CUmodule   module;
+CUfunction function;
+
+CUresult err = cuInit(0);
+
+checkCudaErrors(cuDeviceGet(&device, 0));
+
+err = cuCtxCreate(&context, 0, device);
+if (err != CUDA_SUCCESS) {
+        fprintf(stderr, "* Error initializing the CUDA context.\n");
+        cuCtxDetach(context);
+        exit(-1);
+}
+
+err = cuModuleLoad(&module, "module.ptx");
+if (err != CUDA_SUCCESS) {
+        fprintf(stderr, "* Error loading the module %s\n", module_file);
+        cuCtxDetach(context);
+        exit(-1);
+}
+
+err = cuModuleGetFunction(&function, module, kernel_name);
+
+if (err != CUDA_SUCCESS) {
+        fprintf(stderr, "* Error getting kernel function %s\n", kernel_name);
+        cuCtxDetach(context);
+        exit(-1);
+}
 
 
-func host_function_ptr;
-func (*get_ptr)();
-get_ptr = (func (*)())dlsym( m_handle, "get_ptr_five_times");
-host_function_ptr = get_ptr();
-printf("host function pointer main %p", host_function_ptr);
 
-void * m_handle2 = dlopen("./module.so", RTLD_NOW);
-  if (!m_handle2) { 
-   fprintf(stderr, "dlopen failure: %s\n", dlerror()); 
-   exit (EXIT_FAILURE); }
-//char *errstr;
-errstr = dlerror();
-if (errstr != NULL)
-printf ("A dynamic linking error occurred: (%s)\n", errstr);
-
-func host_function_ptr2;
-func (*get_ptr2)();
-get_ptr2 = (func (*)())dlsym( m_handle2, "get_pointer");
-host_function_ptr2 = get_ptr2();
-printf("host function pointer main %p", host_function_ptr2);
-
-
-void * m_handle3 = dlopen("./module.so", RTLD_NOW);
-  if (!m_handle3) { 
-   fprintf(stderr, "dlopen failure: %s\n", dlerror()); 
-   exit (EXIT_FAILURE); }
-//char *errstr;
-errstr = dlerror();
-if (errstr != NULL)
-printf ("A dynamic linking error occurred: (%s)\n", errstr);
-
-
-void (*launch)(func,func);
-launch= (void(*)(func,func))dlsym( m_handle3, "launch");
-printf("ok.\n");
-if (launch==NULL) {printf("NULL\n");}
-(*launch)(host_function_ptr,host_function_ptr2);
 
 }
