5a6f77a0d364df113d1509a1eecfa35fc162d8df
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-04-17 12:37:09 -0300

    teste dynamic loading

--- testecuda/teste_module.cu
+++ testecuda/teste_module.cu
@@ -23,15 +23,7 @@ get_ptr = (func (*)())dlsym( m_handle, "get_ptr_five_times");
 host_function_ptr = get_ptr();
 printf("host function pointer main %p", host_function_ptr);
 
-
-func host_function_ptr2;
-func (*get_ptr2)();
-get_ptr2 = (func (*)())dlsym( m_handle, "get_pointer");
-host_function_ptr2 = get_ptr2();
-printf("host function pointer main %p", host_function_ptr2);
-
-
-/* void * m_handle2 = dlopen("./module.so", RTLD_NOW);
+void * m_handle2 = dlopen("./module.so", RTLD_NOW);
   if (!m_handle2) { 
    fprintf(stderr, "dlopen failure: %s\n", dlerror()); 
    exit (EXIT_FAILURE); }
@@ -40,10 +32,25 @@ errstr = dlerror();
 if (errstr != NULL)
 printf ("A dynamic linking error occurred: (%s)\n", errstr);
 
-*/
+func host_function_ptr2;
+func (*get_ptr2)();
+get_ptr2 = (func (*)())dlsym( m_handle2, "get_pointer");
+host_function_ptr2 = get_ptr2();
+printf("host function pointer main %p", host_function_ptr2);
+
+
+void * m_handle3 = dlopen("./module.so", RTLD_NOW);
+  if (!m_handle3) { 
+   fprintf(stderr, "dlopen failure: %s\n", dlerror()); 
+   exit (EXIT_FAILURE); }
+//char *errstr;
+errstr = dlerror();
+if (errstr != NULL)
+printf ("A dynamic linking error occurred: (%s)\n", errstr);
+
 
 void (*launch)(func,func);
-launch= (void(*)(func,func))dlsym( m_handle, "launch");
+launch= (void(*)(func,func))dlsym( m_handle3, "launch");
 printf("ok.\n");
 if (launch==NULL) {printf("NULL\n");}
 (*launch)(host_function_ptr,host_function_ptr2);
