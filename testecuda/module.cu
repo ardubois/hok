0e421a50815b41fd0dbe6ac8f3ac08a08b0aa279
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-04-17 12:25:23 -0300

    teste dynamic loading

--- testecuda/module.cu
+++ testecuda/module.cu
@@ -35,6 +35,23 @@ extern "C" pfunc get_pointer()
 }
 
 
+__device__ float five_times(float arg) {
+    return 5 * arg;
+}
+
+// create device function pointer here
+__device__ pfunc five_times_ptr = five_times;
+
+extern "C" pfunc get_ptr_five_times()
+{
+    pfunc host_function_ptr;
+    // copy function pointer value from device to host
+    gpuErrchk(cudaMemcpyFromSymbol(&host_function_ptr, five_times_ptr, sizeof(pfunc)));
+    return host_function_ptr;
+   
+}
+
+
 extern "C" void launch(pfunc myptr)
 {
     // create a host function pointer
