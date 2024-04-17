4afc3289dc4042cc0cc40d8a9a6683721bd5490c
ardubois <ardubois@github.com>
2024-04-17 14:24:29 -0300

    testing .ptx modules

---
+++ ptx_test/module.cu
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+#define gpuErrchk(val) \
+    cudaErrorCheck(val, __FILE__, __LINE__, true)
+void cudaErrorCheck(cudaError_t err, char* file, int line, bool abort)
+{
+    if(err != cudaSuccess)
+    {
+        printf("%s %s %d\n", cudaGetErrorString(err), file, line);
+        if(abort) exit(-1);
+    }
+}
+
+typedef float (*pfunc)(float arg);
+
+__device__ float dev_func(float arg) {
+    return arg * arg;
+}
+
+// create device function pointer here
+__device__ pfunc dev_func_ptr = dev_func;
+
+__global__ void ker_func(pfunc fnc,pfunc func2) {
+    // call function through device function pointer
+    printf("%f\n", func2(fnc(2)));
+}
+
+extern "C" pfunc get_pointer()
+{
+    pfunc host_function_ptr;
+    // copy function pointer value from device to host
+    gpuErrchk(cudaMemcpyFromSymbol(&host_function_ptr, dev_func_ptr, sizeof(pfunc)));
+    return host_function_ptr;
+   
+}
+
+
+__device__ float five_times(float arg) {
+    return 5 * arg;
+}
+
+// create device function pointer here
+__device__ pfunc five_times_ptr = five_times;
+
+extern "C" pfunc get_ptr_five_times()
+{
+    pfunc host_function_ptr;
+    // copy function pointer value from device to host
+    gpuErrchk(cudaMemcpyFromSymbol(&host_function_ptr, five_times_ptr, sizeof(pfunc)));
+    return host_function_ptr;
+   
+}
+
+
+extern "C" void launch(pfunc myptr,pfunc myptr2)
+{
+    // create a host function pointer
+ //   pfunc host_function_ptr;
+    // copy function pointer value from device to host
+   // gpuErrchk(cudaMemcpyFromSymbol(&host_function_ptr, dev_func_ptr, sizeof(pfunc)));
+    // pass the copied function pointer in kernel
+   // printf("my pointer %p\n", myptr);
+   // printf("pointeiro %p\n", host_function_ptr);
+
+    ker_func<<<1,1>>>(myptr,myptr2);
+
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+
+ 
+}
---
+++ ptx_test/teste_module.cu
@@ -0,0 +1,58 @@
+
+#include <stdint.h>
+#include <stdio.h>
+#include <dlfcn.h>
+typedef float (*func)(float);
+
+int main()
+{
+
+ void * m_handle = dlopen("./module2.so", RTLD_NOW);
+  if (!m_handle) { 
+   fprintf(stderr, "dlopen failure: %s\n", dlerror()); 
+   exit (EXIT_FAILURE); }
+char *errstr;
+errstr = dlerror();
+if (errstr != NULL)
+printf ("A dynamic linking error occurred: (%s)\n", errstr);
+
+
+func host_function_ptr;
+func (*get_ptr)();
+get_ptr = (func (*)())dlsym( m_handle, "get_ptr_five_times");
+host_function_ptr = get_ptr();
+printf("host function pointer main %p", host_function_ptr);
+
+void * m_handle2 = dlopen("./module.so", RTLD_NOW);
+  if (!m_handle2) { 
+   fprintf(stderr, "dlopen failure: %s\n", dlerror()); 
+   exit (EXIT_FAILURE); }
+//char *errstr;
+errstr = dlerror();
+if (errstr != NULL)
+printf ("A dynamic linking error occurred: (%s)\n", errstr);
+
+func host_function_ptr2;
+func (*get_ptr2)();
+get_ptr2 = (func (*)())dlsym( m_handle2, "get_pointer");
+host_function_ptr2 = get_ptr2();
+printf("host function pointer main %p", host_function_ptr2);
+
+
+void * m_handle3 = dlopen("./module.so", RTLD_NOW);
+  if (!m_handle3) { 
+   fprintf(stderr, "dlopen failure: %s\n", dlerror()); 
+   exit (EXIT_FAILURE); }
+//char *errstr;
+errstr = dlerror();
+if (errstr != NULL)
+printf ("A dynamic linking error occurred: (%s)\n", errstr);
+
+
+void (*launch)(func,func);
+launch= (void(*)(func,func))dlsym( m_handle3, "launch");
+printf("ok.\n");
+if (launch==NULL) {printf("NULL\n");}
+(*launch)(host_function_ptr,host_function_ptr2);
+
+}
