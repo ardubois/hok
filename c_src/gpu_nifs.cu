ecd13aedc97ca26abdf7d78ae0c0b5e3d3efe34b
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-04-18 11:38:39 -0300

    debug function pointers

--- c_src/gpu_nifs.cu
+++ c_src/gpu_nifs.cu
@@ -392,7 +392,7 @@ static ERL_NIF_TERM load_fun_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM ar
   *kernel_res = ptr;
 
   printf("kernel resource %p", *kernel_res);
-  printf("erlang resource %p", kernel_sers);
+  printf("erlang resource %p", kernel_res);
   
   // We can now make the Erlang term that holds the resource...
   ERL_NIF_TERM term = enif_make_resource(env, kernel_res);
