b9e4c54e8a5322578db347ea8cbebdb7fb502f02
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-04-19 08:48:26 -0300

    debug function pointers

--- map.ex
+++ map.ex
@@ -13,16 +13,33 @@ defmodule PMap do
   end
 end
 
+defmodule PMap2 do
+  import Hok
+  deft sum float -> float ~> float
+  deff sum(a)do
+    return 5*a
+  end
+  deft map2 gmatrex ~> gmatrex ~> gmatrex ~> integer ~> [ float -> float ~> float]  ~> unit
+  defk map2(a1,a2,a3,size,f) do
+    var id int = blockIdx.x * blockDim.x + threadIdx.x
+    if(id < size) do
+      a3[id] = f(a1[id],a2[id])
+    end
+  end
+end
+
 
 n = 10000000
 
 list = [Enum.to_list(1..n)]
 
 vet1 = Matrex.new(list)
+vet2 = Matrex.new(list)
 ref1= Hok.new_gmatrex(vet1)
-ref2= Hok.new_gmatrex(1,n)
+ref2 = Hok.new_gmatrex(vet2)
+ref3= Hok.new_gmatrex(1,n)
 
-map=Hok.load(&PMap.map/4)
+map=Hok.load(&PMap2.map2/4)
 
 threadsPerBlock = 128;
 numberOfBlocks = div(n + threadsPerBlock - 1, threadsPerBlock)
@@ -30,7 +47,7 @@ numberOfBlocks = div(n + threadsPerBlock - 1, threadsPerBlock)
 
 prev = System.monotonic_time()
 
-Hok.spawn(map,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[ref1,ref2,n, Hok.load_fun(&PMap.inc/1)])
+Hok.spawn(map,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[ref1,ref2,n, Hok.load_fun(&PMap2.sum/2)])
 #Hok.synchronize()
 
 next = System.monotonic_time()
