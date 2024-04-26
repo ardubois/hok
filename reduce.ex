a4de5b03e0fd7c784257730768a5f1d092977734
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-04-26 11:49:01 -0300

    teste anonymous functions

--- reduce.ex
+++ reduce.ex
@@ -4,7 +4,12 @@ Hok.defmodule Reduce do
   defh soma(x,y) do
     x + y
   end
-
+  defh max (x,y) do
+    if x > y do
+      return x
+    else
+      return y
+  end
   defk reduce(ref4, a, f,n) do
 
     __shared__ cache[256]
@@ -72,7 +77,7 @@ prev = System.monotonic_time()
 ref1=Hok.new_gmatrex(vet1)
 ref2=Hok.new_gmatrex(vet2)
 
-Hok.spawn(&Reduce.reduce/4,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[ref2, ref1, &Reduce.soma/2,n])
+Hok.spawn(&Reduce.reduce/4,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[ref2, ref1, &Reduce.max/2,n])
 Hok.synchronize()
 
 result_gpu = Hok.get_gmatrex(ref2)
