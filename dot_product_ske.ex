cc72485e9dc275af3c98a774200a4f70c468b772
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-04-29 11:16:03 -0300

    teste anonymous functions

--- dot_product_ske.ex
+++ dot_product_ske.ex
@@ -2,6 +2,7 @@ require Hok
 
 Hok.defmodule PMap2 do
 import Hok
+include CAS
 #deft saxpy float ~> float ~> float
 defh mult(a,b)do
     return a*b
@@ -17,6 +18,55 @@ defh mult(a,b)do
       threadsPerBlock = 128;
       numberOfBlocks = div(size + threadsPerBlock - 1, threadsPerBlock)
       Hok.spawn(&PMap2.map_2kernel/5,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[t1,t2,t3,size,func])
+  end
+  def reduce(ref4, a , f, n) do
+      threadsPerBlock = 256
+      blocksPerGrid = div(n + threadsPerBlock - 1, threadsPerBlock)
+      numberOfBlocks = blocksPerGrid
+      Hok.spawn(&Reduce.reduce/4,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[ref2, ref1, f,n])
+  end
+  defk reduce_ske(ref4, a, f,n) do
+
+    __shared__ cache[256]
+
+    tid = threadIdx.x + blockIdx.x * blockDim.x;
+    cacheIndex = threadIdx.x
+
+    temp =0.0
+
+    if (tid < n) do
+      temp = a[tid]
+      tid = blockDim.x * gridDim.x + tid
+    end
+
+    while (tid < n) do
+      temp = f(a[tid], temp)
+      tid = blockDim.x * gridDim.x + tid
+    end
+
+    cache[cacheIndex] = temp
+      __syncthreads()
+
+    i = blockDim.x/2
+    #tid = threadIdx.x + blockIdx.x * blockDim.x;
+    up = blockDim.x * gridDim.x *256
+    while (i != 0 &&  (cacheIndex + up)< n) do  ###&& tid < n) do
+      #tid = blockDim.x * gridDim.x + tid
+      if (cacheIndex < i) do
+        cache[cacheIndex] = f(cache[cacheIndex + i] , cache[cacheIndex])
+      end
+
+    __syncthreads()
+    i = i/2
+    end
+
+  if (cacheIndex == 0) do
+    current_value = ref4[0]
+    while(!(current_value == atomic_cas(ref4,current_value,f(cache[0],current_value)))) do
+      current_value = ref4[0]
+    end
+  end
+
   end
 end
 
