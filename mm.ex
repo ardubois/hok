e7180744692ed77b77ad657c11e6c07aab003ba6
ardubois <ardubois@github.com>
2024-04-30 14:16:07 -0300

    teste anonymous functions

--- mm.ex
+++ mm.ex
@@ -51,7 +51,7 @@ Hok.include [MM]
 #m = String.to_integer(arg)
 
 m = 1000
-n = m
+#n = m
 k=m
 
 
@@ -69,14 +69,17 @@ mat2 = Matrex.apply(mat,f)
 
 prev = System.monotonic_time()
 
-
-
 result = MM.comp2xy2D(mat1,mat2,1000,1000, Hok.hok fn (mat1,mat2,x,y) ->
-                                      sum = 0.0
-                                      for i in range(0,1000,1) do
-                                              sum = sum + mat1[x * 1000 + i] * mat2[i * 1000 + y]
-                                      end
-                                      sum end)
+           mat1[x * 1000 + i] * mat2[i * 1000 + y]
+  end
+  sum end)
+
+#result = MM.comp2xy2D(mat1,mat2,1000,1000, Hok.hok fn (mat1,mat2,x,y) ->
+#                                      sum = 0.0
+#                                      for i in range(0,1000,1) do
+#                                              sum = sum + mat1[x * 1000 + i] * mat2[i * 1000 + y]
+#                                      end
+#                                      sum end)
 
 next = System.monotonic_time()
 #IO.puts "time gpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
