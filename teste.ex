953bfbf61bada909d9d9638ea13a34420e02e6c0
ardubois <ardubois@github.com>
2024-04-30 16:38:40 -0300

    teste anonymous functions

--- teste.ex
+++ teste.ex
@@ -1,5 +1,3 @@
 require Hok
 
-result = Hok.gpufor x <- 0..1000, mat1, y <- 0..1000, mat2 do v= x* x
-                                                              result = v +y
-                                                               result  end
+result = fn(x,y) -> do x + y end end
