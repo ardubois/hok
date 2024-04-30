dfcf29ae162858c2be2fa00f4d1cf7e55a86efe9
ardubois <ardubois@github.com>
2024-04-30 16:43:15 -0300

    teste anonymous functions

--- lib/hok.ex
+++ lib/hok.ex
@@ -8,6 +8,7 @@ defmodule Hok do
   defmacro hok(function) do
      #resp =  Macro.escape(quote(do: {:anon , unquote(function)}))
      #resp
+    IO.inspect function
     fname = Hok.CudaBackend.gen_lambda("Elixir.App",function)
     result = quote do: Hok.load_lambda(unquote("Elixir.App"), unquote fname)
     IO.inspect result
