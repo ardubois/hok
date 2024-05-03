71fef4a4b5811d524b97192151ee6b4c457d93b4
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-05-03 10:11:10 -0300

    teste anonymous functions

--- examples/nearest_neighbor.ex
+++ examples/nearest_neighbor.ex
@@ -175,4 +175,4 @@ result_elixir = Enum.reverse(NN.euclid_seq(list_data_set,0.0,0.0))
 IO.puts("NN = #{nn[1]}")
 
 
-IO.inspect (Enum.reduce(result_elixir,0, fn (x,y)-> if x>y do x else y end end))
+IO.inspect (Enum.reduce(result_elixir,0, fn (x,y)-> if x<y do x else y end end))
