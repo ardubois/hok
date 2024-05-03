9f779af2b12916a71ca46b9d4450221d9cbd2b46
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-05-03 09:49:57 -0300

    teste anonymous functions

--- examples/nearest_neighbor.ex
+++ examples/nearest_neighbor.ex
@@ -156,7 +156,7 @@ prev = System.monotonic_time()
 
 nn_d = data_set_device
       |> NN.map_step_2para_1resp(2,0.0,0.0,size, &NN.euclid/3)
-      |> DP.reduce(&NN.maior/2)
+      |> NN.reduce(&NN.maior/2)
 
 
 nn = Hok.get_gmatrex(nn_d)
