89576dca3431ad1d5810c540289b7446cd555d33
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-05-05 10:27:33 -0300

    teste anonymous functions

--- lib/hok/type_inference.ex
+++ lib/hok/type_inference.ex
@@ -246,10 +246,13 @@ defmodule Hok.TypeInference do
             end
 
           {fun, _, args} when is_list(args)->
+            IO.puts "case function"
             IO.inspect fun
             IO.inspect args
-            raise "hell"
+            IO.puts "#########"
+           # raise "hell"
              type_fun = map[fun]
+             IO.inspect type_fun
               if( type_fun == nil) do
                   Enum.reduce(args,map, fn v,acc -> infer_type_exp(acc,v) end)
               else
