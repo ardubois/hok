20b44fbf834c22e727a69737706e492d2ee44184
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-05-13 10:05:07 -0300

    teste anonymous functions

--- lib/hok.ex
+++ lib/hok.ex
@@ -421,8 +421,8 @@ def load_fun(fun) do
 
               #IO.puts module
               #raise "hell"
-              #Hok.load_fun_nif(to_charlist(module),to_charlist(funname))
-              Hok.load_fun_nif(to_charlist(:App),to_charlist(funname))
+              Hok.load_fun_nif(to_charlist(module),to_charlist(funname))
+              #Hok.load_fun_nif(to_charlist(:App),to_charlist(funname))
     _ -> raise "Hok.invalid function"
   end
 end
