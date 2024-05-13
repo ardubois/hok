e4f2fc6518ea2cf1c4763da775df1b1ca3b75f49
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-05-13 09:30:49 -0300

    teste anonymous functions

--- c_src/gpu_nifs.cu
+++ c_src/gpu_nifs.cu
@@ -381,6 +381,8 @@ static ERL_NIF_TERM load_fun_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM ar
   void* (*fn)();
   fn= (void* (*)())dlsym( m_handle, func_name);
   
+  printf("pointer function %p\n",fn);
+
   if(fn == NULL)  
       { char message[200];
         strcpy(message,"Error opening function from dll!!!");
