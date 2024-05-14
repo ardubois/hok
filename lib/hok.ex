be946d3ed31479dbf9087947e73c543b5ee8f69d
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-05-14 13:45:39 -0300

    teste anonymous functions

--- lib/hok.ex
+++ lib/hok.ex
@@ -416,7 +416,7 @@ def load_type(kernel) do
               #map["#{module_name}_#{kernelname}"]
               IO.inspect map
               IO.inspect "#{kernelname}"
-              resp = map["#{kernelname}"]
+              resp = Map.get(map,"#{kernelname}")
               IO.inspect resp
               resp
     _ -> raise "Hok.build: invalid kernel"
