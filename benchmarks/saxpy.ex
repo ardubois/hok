fcf3e5fae8a42919902c26b43e7a591b016f9c95
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-05-22 08:43:50 -0300

    compilation at run time using substitution

---
+++ benchmarks/saxpy.ex
@@ -0,0 +1,54 @@
+require Hok
+
+Hok.defmodule PMap2 do
+deft saxpy float ~> float ~> float
+defh saxpy(a,b)do
+    return 2*a+b
+  end
+  deft map_2kernel gmatrex ~> gmatrex ~> gmatrex ~> integer ~> [ float ~> float ~> float]  ~> unit
+  defk map_2kernel(a1,a2,a3,size,f) do
+    var id int = blockIdx.x * blockDim.x + threadIdx.x
+    var stride int = blockDim.x * gridDim.x;
+
+    for i in range(id,size,stride) do
+      if(id < size) do
+       a3[id] = f(a1[id],a2[id])
+      end
+    end
+  end
+  def map2(t1,t2,t3,size,func) do
+      threadsPerBlock = 128;
+      numberOfBlocks = div(size + threadsPerBlock - 1, threadsPerBlock)
+      Hok.spawn_jit(&PMap2.map_2kernel/5,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[t1,t2,t3,size,func])
+  end
+end
+
+Hok.include [PMap2]
+
+n = 10000000
+
+list = [Enum.to_list(1..n)]
+
+vet1 = Matrex.new(list)
+vet2 = Matrex.new(list)
+
+prev = System.monotonic_time()
+
+ref1= Hok.new_gmatrex(vet1)
+ref2 = Hok.new_gmatrex(vet2)
+ref3= Hok.new_gmatrex(1,n)
+
+
+
+
+PMap2.map2(ref1,ref2,ref3,n, &PMap2.saxpy/2)
+#PMap2.map2(ref1,ref2,ref3,n, Hok.hok(fn (a,b) -> type a float; type b float; return 2*a+b end))
+#PMap2.map2(ref1,ref2,ref3,n, Hok.hok(fn (a,b) -> 2*a+b end))
+
+#Hok.synchronize()
+
+next = System.monotonic_time()
+IO.puts "time gpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
+
+result = Hok.get_gmatrex(ref3)
+IO.inspect result
