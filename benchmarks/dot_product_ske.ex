b5f415012adeed4378114c32f7166b51957b26c8
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-06-01 17:49:49 -0300

    compilation at run time using substitution

--- benchmarks/dot_product_ske.ex
+++ benchmarks/dot_product_ske.ex
@@ -128,6 +128,6 @@ result = Hok.get_gmatrex(result_gpu)
 
 
 next = System.monotonic_time()
-IO.puts "Hok #{System.convert_time_unit(next-prev,:native,:millisecond)}"
+IO.puts "Hok\t#{n}\t#{System.convert_time_unit(next-prev,:native,:millisecond)}"
 
 IO.inspect result
