09678b669a5a97eca54729408282ef61faefa588
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-06-14 15:17:43 -0300

    compilation at run time using substitution

--- paper_ex2.ex
+++ paper_ex2.ex
@@ -1,10 +1,10 @@
 require Hok
 Hok.defmodule Ex1 do
-  deft square float ~> float
+  #deft square float ~> float
   defh square(x)do
      x*x
   end
-  deft apply_k gmatrex ~> gmatrex ~> integer ~> [ float ~> float]  ~> unit
+  #deft apply_k gmatrex ~> gmatrex ~> integer ~> [ float ~> float]  ~> unit
   defk apply_k(a,r,size,f) do
    var id int  = blockIdx.x * blockDim.x + threadIdx.x
    if(id < size) do
