a76f4702a9de907f31ba35b559663247f41bc21c
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-06-14 15:05:02 -0300

    compilation at run time using substitution

---
+++ paper_ex2.ex
@@ -0,0 +1,42 @@
+require Hok
+Hok.defmodule Ex1 do
+  #deft inc float ~> float
+  defh inc(a)do
+     1+a
+  end
+  #deft apply_k gmatrex ~> gmatrex ~> integer ~> [ float ~> float]  ~> unit
+  defk apply_k(a,r,size,f) do
+   var id int = blockIdx.x * blockDim.x + threadIdx.x
+   if(id < size) do
+      r[id] = f(a[id])
+    end
+  end
+
+end
+
+Hok.include [Ex1]
+
+n = 10000000
+
+list = [Enum.to_list(1..n)]
+
+vet1 = Matrex.new(list)
+vet1_gpu= Hok.new_gmatrex(vet1)
+resp_gpu= Hok.new_gmatrex(1,n)
+
+#map=Hok.load(&PMap.map2/4)
+
+threadsPerBlock = 128;
+numberOfBlocks = div(n + threadsPerBlock - 1, threadsPerBlock)
+
+
+prev = System.monotonic_time()
+
+Hok.spawn(&Ex1.apply_k/4,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[vet1_gpu,resp_gpu,n, &Ex1.inc/1])
+#Hok.synchronize()
+
+next = System.monotonic_time()
+IO.puts "time gpu #{System.convert_time_unit(next-prev,:native,:millisecond)}"
+
+result = Hok.get_gmatrex(resp_gpu)
+IO.inspect result
