0494a043ce624deafd58093f81e9b435a3f20df2
ardubois <ardubois@github.com>
2024-08-07 14:35:25 -0300

    adding nx support

--- teste.ex
+++ teste.ex
@@ -1,9 +1,9 @@
 
 
 
-t = Nx.tensor([[1, 2, 3, 4]],type: {:f, 32})
+#t = Nx.tensor([[1, 2, 3, 4]],type: {:f, 32})
 
-#t = Matrex.new([[1,2,3,4]])
+t = Matrex.new([[1,2,3,4]])
 
 gm = Hok.new_gmatrex(t)
 
