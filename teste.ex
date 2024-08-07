e296da64ada1ee9336863791e9097417d184286e
ardubois <ardubois@github.com>
2024-08-07 14:34:36 -0300

    adding nx support

--- teste.ex
+++ teste.ex
@@ -3,9 +3,11 @@
 
 t = Nx.tensor([[1, 2, 3, 4]],type: {:f, 32})
 
-t = Matrex.new([[1,2,3,4]])
+#t = Matrex.new([[1,2,3,4]])
 
 gm = Hok.new_gmatrex(t)
+
+IO.inspect gm
 
 mt = Hok.get_gmatrex(gm)
 
