09239c078ffad66f2f503127de0701e161d993b9
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-21 14:21:09 -0300

    typed version

--- benchmarks/saxpy.ex
+++ benchmarks/saxpy.ex
@@ -31,14 +31,19 @@ Hok.include [PMap2]
 n = String.to_integer(arg)
 
 
-vet1 = Matrex.new(1, n, fn -> :rand.uniform() end)
-vet2 = Matrex.new(1, n, fn -> :rand.uniform() end)
+#vet1 = Matrex.new(1, n, fn -> :rand.uniform() end)
+#vet2 = Matrex.new(1, n, fn -> :rand.uniform() end)
+
+vet1 = Hok.new_nx_from_function(1,n,{:f,32},fn -> :rand.uniform(1000) end )
+vet2 = Hok.new_nx_from_function(1,n,{:f,32},fn -> :rand.uniform(1000) end)
+
+
 
 prev = System.monotonic_time()
 
-ref1= Hok.new_gmatrex(vet1)
-ref2 = Hok.new_gmatrex(vet2)
-ref3= Hok.new_gmatrex(1,n)
+ref1= Hok.new_gnx(vet1)
+ref2 = Hok.new_gnx(vet2)
+ref3= Hok.new_gnx(1,n)
 
 
 
