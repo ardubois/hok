93742b019cd61789197551478c1c526f5ae9f1d4
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-21 09:57:31 -0300

    Nx operations

--- teste.ex
+++ teste.ex
@@ -12,10 +12,10 @@ t = Nx.tensor([[1, 2, 3, 4]],type: {:f, 32})
 
 #t = Matrex.new([[1,2,3,4]])
 
-gm = Hok.new_gmatrex(t)
+gm = Hok.new_gnx(t)
 
 IO.inspect gm
 
-mt = Hok.get_gmatrex(gm)
+mt = Hok.get_gnx(gm)
 
 IO.inspect mt
