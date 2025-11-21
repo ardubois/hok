198ee67a90cd78102dea928de7e39cda2d1acf73
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-21 14:44:24 -0300

    typed version

--- lib/hok/cuda_backend.ex
+++ lib/hok/cuda_backend.ex
@@ -150,6 +150,8 @@ end
     {fname, iinfo, para} = header
     {delta,is_typed}  = if(is_tuple(type_def)) do
         types = get_type_fun(type_def)
+        IO.puts "type"
+        IO.inspect types
         delta= para
           |> Enum.map(fn({p, _, _}) -> p end)
           |> Enum.zip(types)
