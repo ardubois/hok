66268e6ce75fcb2cfa2eaa741862d610b200b1b7
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-21 13:01:14 -0300

    type inference

--- lib/hok.ex
+++ lib/hok.ex
@@ -621,10 +621,7 @@ defp process_args_no_fun([]), do: []
 ### first two arguments are used for error messages. Takes a list of types and a list of actual parameters and type checks them
 
 
-{:f,32} -> create_gpu_array_nx_nif(array,l,c,Kernel.to_charlist("float"))
-     {:f,64} -> create_gpu_array_nx_nif(array,l,c,Kernel.to_charlist("double"))
-     {:s,32} 
-
+{
 def type_check_args(kernel,narg, [:matrex | t1], [a|t2]) do
     case a do
       {_ref,{_l,_c}} -> type_check_args(kernel,narg+1,t1,t2)
