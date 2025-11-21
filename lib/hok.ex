0f7a64e528004de4cba6b0e3240fe0be7efb7e7b
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-21 13:01:43 -0300

    type inference

--- lib/hok.ex
+++ lib/hok.ex
@@ -621,7 +621,7 @@ defp process_args_no_fun([]), do: []
 ### first two arguments are used for error messages. Takes a list of types and a list of actual parameters and type checks them
 
 
-{
+
 def type_check_args(kernel,narg, [:matrex | t1], [a|t2]) do
     case a do
       {_ref,{_l,_c}} -> type_check_args(kernel,narg+1,t1,t2)
