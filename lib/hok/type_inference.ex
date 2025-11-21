fa8dfdf80a80a1df03bc15b2a3e29f1509f0bd8c
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-21 11:52:42 -0300

    type inference

--- lib/hok/type_inference.ex
+++ lib/hok/type_inference.ex
@@ -134,7 +134,7 @@ defmodule Hok.TypeInference do
 
 
   def get_default_type() do
-    send(:types_server,{:get_default_type, self()})
+    send(:types_ast_server,{:get_default_type, self()})
     receive do
                {:default_type, type} -> type
                h    -> raise "Unknown message from type server #{inspect h}"
