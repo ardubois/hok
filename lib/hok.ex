648b08474738be220efda559bf774609ebdf5b8b
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-22 11:04:21 -0300

    new compilation

--- lib/hok.ex
+++ lib/hok.ex
@@ -127,7 +127,7 @@ defmodule Hok do
     defmacro include_rts(inc_list) do
       #IO.inspect inc_list
       includes = inc_list
-                  |> Enum.map(fn {_,_,[module]} -> to_string(module) end)
+                  |> Enum.map(fn {_,_,[module]} -> module end)
                   |> Enum.map(fn module -> add_module_to_app(module) end)
   
     end
