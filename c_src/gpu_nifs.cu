6dc4a947069c35b8313cb8df28a5457af3bea3a2
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-22 11:33:09 -0300

    new compilation

--- c_src/gpu_nifs.cu
+++ c_src/gpu_nifs.cu
@@ -846,7 +846,7 @@ static ERL_NIF_TERM load_lib_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM ar
   *lib_res = m_handle;
   
   // We can now make the Erlang term that holds the resource...
-  ERL_NIF_TERM term = enif_make_resource(env, kernel_res);
+  ERL_NIF_TERM term = enif_make_resource(env, lib_res);
   // ...and release the resource so that it will be freed when Erlang garbage collects
   enif_release_resource(kernel_res);
  
