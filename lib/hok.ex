ee3a59c0d5241d89544d4a4e50fac3636dc0b005
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-23 08:33:03 -0300

    new compilation

--- lib/hok.ex
+++ lib/hok.ex
@@ -177,7 +177,7 @@ defmodule Hok do
     lib=Hok.load_lib_nif(to_charlist("Elixir.app"))
 
     k=Hok.load_kernel_from_lib_nif(to_charlist("Elixir.app"),to_charlist("#{kernel_name}"),lib)
-    args = Hok.process_args_lib(unquote l,lib)
+    args = Hok.process_args_lib((unquote l),lib)
     IO.inspect args
     Hok.spawn_nif(k,unquote(t),unquote(b),args)
 
