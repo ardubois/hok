a98bc59c7254afb94d2f1c6cb3f0a57bc424fa48
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-23 07:26:16 -0300

    new code generation

--- lib/hok.ex
+++ lib/hok.ex
@@ -173,9 +173,9 @@ defmodule Hok do
     
     lib=Hok.load_lib_nif(to_charlist("Elixir.app"))
     k=Hok.load_kernel_from_lib_nif(to_charlist("Elixir.app"),to_charlist("#{unquote f_name}"),lib)
-    args = Hok.process_args(l)
+    args = Hok.process_args(unquote l)
 
-    Hok.spawn_nif(k,t,b,args)
+    Hok.spawn_nif(unquote k,unquote t,unquote b,args)
 
     IO.puts "Ok!"
   end
