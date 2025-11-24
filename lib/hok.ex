ee9fad087bac6801403b7591de7531ae66fed4bd
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-24 06:34:06 -0300

    new compilation

--- lib/hok.ex
+++ lib/hok.ex
@@ -187,7 +187,7 @@ defmodule Hok do
      f_name= case Macro.escape(k) do
        {:&, [_l1],[{:/, [_l2], [{{:., [_l3], [_module, f_name]}, [no_parens: true], [_l4]}, _nargs]}]} -> f_name
        {:&, _ ,[{:/, _,  [{{:., _, [{:__aliases__, _, [module]}, kernelname]}, _, []}, _nargs]}]} -> kernelname
-        _ -> raise "Argument to spawn should be a function."
+        v -> raise "Argument to spawn should be a function: #{inspect v}."
       
      end
    
