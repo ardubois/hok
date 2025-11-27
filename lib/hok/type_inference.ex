5b8a9c301bafacd99ac054c283ed65f06a73a232
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-27 20:21:32 -0300

    new compilation

--- lib/hok/type_inference.ex
+++ lib/hok/type_inference.ex
@@ -92,7 +92,8 @@ defmodule Hok.TypeInference do
           {:return,_,_} -> com
           {:if, info, [ exp,[do: block]]} -> {:if, info, [ exp,[do: check_return block]]}
           {:if, info, [ exp,[do: block, else: belse ]]} -> {:if, info, [ exp,[do: check_return(block), else: check_return(belse) ]]}
-          {:=, info, args} ->  [ {:=, info, args},  {:return,[],[:unit]}]
+          {:=, info, args} -> raise "hell"  
+            [ {:=, info, args},  {:return,[],[:unit]}]
           _ -> if is_exp?(com) do
                       {:return,[],[com]}
                   else
