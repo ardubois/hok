4f97c27e46805666a689dcf9357193007b35aec8
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-28 13:17:21 -0300

    new compilation

--- benchmarks/cola/nbodies.ex
+++ benchmarks/cola/nbodies.ex
@@ -149,7 +149,7 @@ r1 = NBodies.map_2_para_no_resp(d_buf,d_buf,nBodies,nBodies, &NBodies.gpu_nBodie
 Hok.set_default_type(%{default: :double, a: :double, b: :double, c: :int })
 
 r2 = NBodies.map_2_para_no_resp( r1, 0.01,nBodies,nBodies, &NBodies.gpu_integrate/3)
-  |> PolyHok.get_gnx
+  |> Hok.get_gnx
   #|> IO.inspect
 
   next = System.monotonic_time()
