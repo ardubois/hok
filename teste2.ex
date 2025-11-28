be9a74b6370e3c1580cc1fa6c59c919f2b194f1b
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-27 21:14:48 -0300

    new compilation

--- teste2.ex
+++ teste2.ex
@@ -3,7 +3,7 @@ require Hok
 Hok.defmodule Teste do
 
 defd teste_kernel(a) do
-   #index = blockIdx.x * blockDim.x + threadIdx.x;
+   index = blockIdx.x * blockDim.x + threadIdx.x;
    a[1] = a[1] + 1
  end
 end
