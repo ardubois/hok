6716e3c51d175cfea0cd2a1c752fffc8e55d9581
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-28 09:16:57 -0300

    new compilation

--- lib/hok.ex
+++ lib/hok.ex
@@ -263,7 +263,7 @@ end
 
     app = get_app()
     
-    code = Hok.CudaBackend.compile_module(:app, app, quote do: type)
+    code = Hok.CudaBackend.compile_module(:app, app, unquote type)
     module_name = "Elixir.app_#{to_string(type)}"
     IO.puts "Module name: #{module_name}"
          #IO.puts "Module name: #{module_name}"
