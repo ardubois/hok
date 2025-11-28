87a8c2b80b90f48a6e36d0d2592e0fe3cc3c2988
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-28 09:52:00 -0300

    new compilation

--- lib/hok/type_inference.ex
+++ lib/hok/type_inference.ex
@@ -565,7 +565,8 @@ defp set_type_exp(map,type,exp) do
               :none ->      {map, infered_type}= infer_types_args(map,args,[])
                             map = Map.put(map,fun, {type,infered_type})
                             map
-              {ret,type_args} -> {map, infered_type} = set_type_args(map,type_args,args,[])
+              {ret,type_args} -> IO.puts "set type args call fun: #{fun}, args: #{inspect args}" 
+                              {map, infered_type} = set_type_args(map,type_args,args,[])
                               cond do
                                 ret == type -> Map.put(map,fun, {type, infered_type})
                                 ret == :none -> Map.put(map,fun, {type, infered_type})
