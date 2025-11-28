c318baf068a20a0ed11004866947b3048a9deccd
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-11-27 21:13:12 -0300

    new compilation

---
+++ teste2.ex
@@ -0,0 +1,30 @@
+require Hok
+
+Hok.defmodule Teste do
+
+defk teste_kernel(a) do
+   #index = blockIdx.x * blockDim.x + threadIdx.x;
+   a[index] = a[index] + 1
+ end
+end
+
+Hok.include_rts [Teste]
+Hok.set_default_type(:float)
+
+
+
+t = Nx.tensor([[1, 2, 3, 4]],type: {:s, 32})
+
+#t = Matrex.new([[1,2,3,4]])
+size = 4
+gm = Hok.new_gnx(t)
+
+IO.inspect gm
+
+threadsPerBlock = 128;
+numberOfBlocks = div(size + threadsPerBlock - 1, threadsPerBlock)
+Hok.spawn(&Teste.teste_kernel/1,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[gm])
+
+mt = Hok.get_gnx(gm)
+
+IO.inspect mt
