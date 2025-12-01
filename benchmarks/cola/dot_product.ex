7ab956a6e404203186ba2251d668644c47ddc4d0
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-12-01 13:09:11 -0300

    new compilation

--- benchmarks/cola/dot_product.ex
+++ benchmarks/cola/dot_product.ex
@@ -4,7 +4,7 @@ require Integer
 #import Nx
 Hok.defmodule_rts DP do
 include CAS
-  deft (arr a) ~> (arr b) ~> (arr c) ~> integer ~> [a ~> b ~> c]
+  deft map_2kernel (arr a) ~> (arr b) ~> (arr c) ~> integer ~> [a ~> b ~> c]
   defk map_2kernel(a1,a2,a3,size,f) do
     id = blockIdx.x * blockDim.x + threadIdx.x
     if(id < size) do
