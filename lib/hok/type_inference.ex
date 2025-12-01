8eb0e048d521697d52022877105b334ff49ec066
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-12-01 08:24:36 -0300

    new compilation

--- lib/hok/type_inference.ex
+++ lib/hok/type_inference.ex
@@ -498,6 +498,7 @@ defp set_type_exp(map,type,exp) do
           ntype ->  set_type_exp(map,ntype,arg1)
                     case t2 do
                         :none -> map = set_type_exp(map,ntype,arg2)
+                                 map
                         ntype2 -> if ntype != ntype2 do
                                       raise "Operator #{inspect op} (#{inspect info}) is applyed to type #{t1} and type #{t2}."
                                   else
