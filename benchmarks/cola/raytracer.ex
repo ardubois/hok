1fa1ff33038c96b7c7f371aadc9a11de37216a66
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-12-01 08:22:42 -0300

    new compilation

--- benchmarks/cola/raytracer.ex
+++ benchmarks/cola/raytracer.ex
@@ -175,7 +175,7 @@ defmodule Main do
         ref_sphere = Hok.new_gnx(sphereList)
         ref_image = Hok.new_gnx(1,width * height  * 4,{:s,32})
 
-        Hok.set_default_type(%{default:  :int, a: :int, b: :tflaot, c: :int })
+        Hok.set_default_type(%{default:  :int, a: :int, b: :tfloat, c: :int })
 
         RayTracer.mapxy_2D_para_no_resp(ref_image, 4,width, ref_sphere, width, &RayTracer.raytracing/5)
 
