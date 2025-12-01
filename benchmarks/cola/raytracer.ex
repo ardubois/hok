1ff31756657eb4a2e81e9b7d8be6ee5655ebfca4
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-12-01 09:03:38 -0300

    new compilation

--- benchmarks/cola/raytracer.ex
+++ benchmarks/cola/raytracer.ex
@@ -175,13 +175,13 @@ defmodule Main do
         ref_sphere = Hok.new_gnx(sphereList)
         ref_image = Hok.new_gnx(1,width * height  * 4,{:s,32})
 
-        Hok.set_default_type(%{default:  :int, a: :int, b: :tfloat, c: :int })
-        IO.inspect "before"
+        Hok.set_default_type(%{default:  :int, a: :int, b: :int, c: :tfloat })
+      #  IO.inspect "before"
         RayTracer.mapxy_2D_para_no_resp(ref_image, 4,width, ref_sphere, width, &RayTracer.raytracing/5)
-        IO.inspect "after"
+      #  IO.inspect "after"
        # Hok.spawn_jit(&RayTracer.raytracing/4,{trunc(width/16),trunc(height/16),1},{16,16,1},[width, height, refSphere, refImag])
 
-        image = Hok.get_gnx(ref_image)
+        _image = Hok.get_gnx(ref_image)
 
         next = System.monotonic_time()
         IO.puts "Hok\t#{width}\t#{System.convert_time_unit(next-prev,:native,:millisecond)} "
